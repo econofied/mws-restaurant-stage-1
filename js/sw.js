085daa008b9dd1f1948dd35d406d94875db3782c
Joshua Pickard <joshuapickard.jp@gmail.com>
2018-11-04 13:14:21 -0500

    service worker

--- js/sw.js
+++ js/sw.js
@@ -1,7 +1,7 @@
 console.log('Service Worker: Registered');
 self.addEventListener('install', function(e) {
 	e.waitUntil(
-		caches.open(.'v1').then(function(cache) {
+		caches.open('v1').then(function(cache) {
 			return cache.addAll(cacheFiles);
 		})
 
